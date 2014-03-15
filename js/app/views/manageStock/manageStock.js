d18b168fb94deee6d14f1459fd66eb61fa17dbbe
yedukondalu <yedukondalurd@gmail.com>
2014-03-15 18:34:38 +0530

    Signed-off-by: yedukondalu <yedukondalurd@gmail.com>

--- js/app/views/manageStock/manageStock.js
+++ js/app/views/manageStock/manageStock.js
@@ -7,12 +7,14 @@ define(function (require) {
     var Backbone = require('backbone');
     var _ = require('underscore');
     var manageStock = require('text!tpl/manage-stock/index.html');
+    var itemHtml = require('text!tpl/manage-stock/add/index.html');
     return Backbone.View.extend({
         events: {
             "click .item_buttons .add_item_button": "addItem"
         },
         el: '#application-content-host',
         template: _.template(manageStock),
+        itemTemplate: _.template(itemHtml),
         init: function () {
             console.log('Manage stock initialized');
             var tableCollection = {};
@@ -27,10 +29,12 @@ define(function (require) {
              {bSortable: !1, aTargets: [0, 6]}
              ], aaSorting: []});*/
             require('jquery-ui');
-            $("#loginDialog").dialog({
+            $("#add-item-form").dialog({
                 autoOpen: false,
+                width: 380,
+                modal: true,
                 buttons: {
-                    "Login": function () {
+                    "Add Item": function () {
                         $(this).dialog("close");
                     }
                 },
@@ -44,18 +48,43 @@ define(function (require) {
                     easing: "easeOutBack"
                 }
             });
-
         },
         render: function (tableCollection) {
-            $(this.$el).html(this.template(tableCollection));
+            $(this.$el).html(this.template({items: tableCollection, itemTemplate: this.itemTemplate}));
         },
         addItem: function (e) {
             console.log($(e));
-            $("#loginDialog").dialog("open");
+            $("#add-item-form").dialog("open");
+            var availableTags = [
+                "ActionScript",
+                "AppleScript",
+                "Asp",
+                "BASIC",
+                "C",
+                "C++",
+                "Clojure",
+                "COBOL",
+                "ColdFusion",
+                "Erlang",
+                "Fortran",
+                "Groovy",
+                "Haskell",
+                "Java",
+                "JavaScript",
+                "Lisp",
+                "Perl",
+                "PHP",
+                "Python",
+                "Ruby",
+                "Scala",
+                "Scheme"
+            ];
+            $("input[name=item_category]").autocomplete({source: availableTags});
+            setTimeout(function () {
+                $('.ui-dialog').after($('.ui-autocomplete'));
+            }, 500);
             e.preventDefault();
         }
 
-    })
-        ;
-})
-;
+    });
+});
